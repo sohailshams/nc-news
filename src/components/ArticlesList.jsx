dc26c6c6539b76fb25df3f2dc50393f7cfd3d7a9
sohailshams <sohailshamas@yahoo.com>
2023-05-30 19:42:54 +0100

    Added a loader while Articles are not fetched.

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -4,24 +4,30 @@ import Article from "./Article";
 
 const ArticlesList = () => {
   const [articlesList, setArticlesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetchArtilces().then((articles) => setArticlesList(articles));
+    setIsLoading(false);
   }, []);
 
   return (
     <>
-      {articlesList.map(
-        ({ article_id, article_img_url, title, author, created_at }) => {
-          return (
-            <Article
-              key={article_id}
-              article_img_url={article_img_url}
-              title={title}
-              author={author}
-              created_at={created_at}
-            />
-          );
-        }
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        articlesList.map(
+          ({ article_id, article_img_url, title, author, created_at }) => {
+            return (
+              <Article
+                key={article_id}
+                article_img_url={article_img_url}
+                title={title}
+                author={author}
+                created_at={created_at}
+              />
+            );
+          }
+        )
       )}
     </>
   );
