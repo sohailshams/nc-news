e8c989e18e63dc2f2d8ebd6f72c5c27ddc60d281
sohailshams <sohailshamas@yahoo.com>
2023-06-02 12:04:26 +0100

    Added spinner while articles are being loaded.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import ArticlesContainer from "./components/ArticlesContainer";
 import Nav from "./components/Nav";
-import ArticleDetail from "./components/ArticleDetail";
 import SingleArticle from "./components/SingleArticle";
 
 function App() {
