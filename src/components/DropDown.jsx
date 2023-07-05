2fe05917b81dac69e6149d8ae1a91493d0fb48ca
sohailshams <sohailshamas@yahoo.com>
2023-07-05 13:27:09 +0100

    Installed headlessui/react, heroicons/react and added dropdown menu.

---
+++ src/components/DropDown.jsx
@@ -0,0 +1,74 @@
+import { Fragment, useContext } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { UserCircleIcon } from "@heroicons/react/20/solid";
+import { UserContext } from "../contexts/User";
+import { Link } from "react-router-dom";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const DropDown = () => {
+  const { setUser } = useContext(UserContext);
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button>
+          <UserCircleIcon
+            className="-mr-1 h-10 w-10 text-gray-400"
+            aria-hidden="true"
+          />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <Link to="/signin">
+                  <button
+                    onClick={() => setUser("tickle122")}
+                    className={classNames(
+                      active
+                        ? "bg-gray-100 text-gray-900 w-full text-left"
+                        : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    Sign In
+                  </button>
+                </Link>
+              )}
+            </Menu.Item>
+
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  onClick={() => setUser(null)}
+                  type="submit"
+                  className={classNames(
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block w-full px-4 py-2 text-left text-sm"
+                  )}
+                >
+                  Sign Out
+                </button>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+};
+
+export default DropDown;
